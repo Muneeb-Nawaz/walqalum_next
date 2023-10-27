a29ea4166b32d63e52a0ded9732df4dbf0a0e258
GeeksforGeeks <GFGexample@gmail.orgg>
2023-10-27 15:45:40 +0500

    setup

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { CarouselCard } from "./Carousel";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar />
+      <CarouselCard />
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Components/index.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Wrapper = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Wrapper;
